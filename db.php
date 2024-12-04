15d90f902c4c558018fbab16b2674b9b257500a8
pixelIq17 <pixeliq.17@gmail.com>
2024-12-04 04:19:43 -0500

    db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conn = new PDO('mysql:host=13.58.47.110;dbname=myweb', 'root', '');
+    $conn = new PDO('mysql:host=18.116.63.100;dbname=myweb', 'root', '');
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error de conexion: " . $e->getMessage());
